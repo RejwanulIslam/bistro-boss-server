a9362de0bf69355b1d57f21dc9b8f4084b915057
Rejwanul Islam <RejwanulIslam@users.noreply.github.com>
2025-06-27 23:35:24 +0600

    query MongoDB for cart items using user email via GET /carts

--- index.js
+++ index.js
@@ -26,13 +26,27 @@ async function run() {
     try {
         const menuCollection = client.db('bistroDB').collection('menu')
         const reviewsCollection = client.db('bistroDB').collection('reviews')
+        const cartsCollection = client.db('bistroDB').collection('carts')
 
-        app.get('/menu',async (req, res) => {
-            const result= await menuCollection.find().toArray()
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray()
             res.send(result)
         })
-        app.get('/reviews',async (req, res) => {
-            const result= await reviewsCollection.find().toArray()
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray()
+            res.send(result)
+        })
+
+        //Carts Collection
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartsCollection.insertOne(cartItem)
+            res.send(result)
+        })
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await cartsCollection.find(query).toArray()
             res.send(result)
         })
 
